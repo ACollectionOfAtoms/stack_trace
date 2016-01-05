ff1b97071dc03d0eead5affdb24c203dd6fa54d1
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-01-04 20:28:34 -0600

    Added /data endpoint

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, jsonify
 import screenscrape as sc
 
 DEBUG = True
@@ -7,19 +7,25 @@ SECRET_KEY = 'super secret key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+queries = {}
 
 @app.route('/')
 def show_results():
-    return render_template('results.html')
+    return render_template('layout.html')
 
 
-@app.route('/results', methods=['POST'])
+@app.route('/results', methods=['POST', 'GET'])
 def generate_graph():
     if request.method == 'POST':
         queries = request.get_json()
         for item in queries:
-            print queries[item]['query'], sc.return_results(queries[item]['query'])
-    return redirect(url_for('show_results'))
+            queries[item]['hits'] = sc.return_results(queries[item]['query'])
+
+
+@app.route('/data', methods=['GET'])
+def send_hits():
+    return jsonify(queries)
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -35,7 +35,23 @@ var stackList = [],
           data: JSON.stringify(queries),
           dataType: "json",
         });
-    };
+    }
+    createGraph = function() {
+      var width = 960,
+          height = 700,
+          format = d3.format(",d"); //convert value to integer
+          color = d3.scale.category20(); // create ordinal scale with 20 colors
+          sizeOfRadius = d3.scale.pow().domain([-100,100]).range([-50,50]);
+      var bubble = d3.layout.pack()
+          .sort(null) // disable sorting, use DOM tree traversal
+          .size([width, height])
+          .padding(1) //padding between circles
+          .radius(function(d){return 20 + (sizeOfRadius(d) * 30); });
+      var svg = d3.select("#chart").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("class", "bubble");
+    }
 
 $(document).ready(function() {
   addButton.click(function() {
